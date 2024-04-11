033bd0fe512167c9e1be6be36b4b047a79de80cd
EugenioDV <esnow.dv@gmail.com>
2024-04-11 18:24:52 +0200

    wip smooth camera chaser interpolation of ALL PARAMS and view target transition (see description)
    Hard to get it right with view blend because when sequences change and the target camera dies, the view target switches to default which is not our chaser. Perhaps I could derive from pawn?
    
    I could:
    - register to on destroy of target camera when switching view target
    - When a new camera is chosen, unregister
    
    This could still yield problems
    
    How to set smooth chaser camera as default view target?
    Where is the code that changes view target when the one we are using is destroyed?

--- Plugins/SequencePlayTools/Source/SequencePlayTools/Private/SmoothChaserCamera.cpp
+++ Plugins/SequencePlayTools/Source/SequencePlayTools/Private/SmoothChaserCamera.cpp
@@ -1,8 +1,74 @@
 ﻿// SmoothChaserCamera.cpp
 #include "SmoothChaserCamera.h"
 
-void ASmoothChaserCamera::SetTargetCamera_Implementation(ACineCameraActor* NewTargetCamera, bool bSnapImmediately, float InterpolationTime)
+void ASmoothChaserCamera::SetTargetCamera(ACineCameraActor* NewTargetCamera, bool bSnapImmediately, float InterpolationTime)
 {
 	TargetCamera = NewTargetCamera;
-	UE_LOG(LogTemp, Warning, TEXT("Called native implementation that is supposed to be handled in blueprints for ASmoothChaserCamera"));
+	CameraSnapshot = FCameraSettingsSnapshot(GetCineCameraComponent());
+	OnTargetCameraChanged(bSnapImmediately, InterpolationTime);
+}
+
+void ASmoothChaserCamera::LerpCameraParams(float Alpha)
+{
+	if (!IsValid(TargetCamera) || !IsValid(TargetCamera->GetCineCameraComponent()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LerpCameraParams: Target Camera invalid."));
+		return;
+	}
+
+	// FilmbackSettings start
+	FCameraFilmbackSettings TargetFilmback = TargetCamera->GetCineCameraComponent()->Filmback;
+
+	TargetFilmback.SensorWidth = FMath::Lerp(CameraSnapshot.FilmbackSettings.SensorWidth, TargetFilmback.SensorWidth, Alpha);
+	TargetFilmback.SensorHeight = FMath::Lerp(CameraSnapshot.FilmbackSettings.SensorHeight, TargetFilmback.SensorHeight, Alpha);
+	TargetFilmback.SensorAspectRatio = FMath::Lerp(CameraSnapshot.FilmbackSettings.SensorAspectRatio, TargetFilmback.SensorAspectRatio, Alpha);
+
+	// todo disabled GetCineCameraComponent()->SetFilmback(TargetFilmback);
+	// FilmbackSettings end
+
+	// LensSettings start
+	FCameraLensSettings TargetLens = TargetCamera->GetCineCameraComponent()->LensSettings;
+	
+	TargetLens.MinFocalLength = FMath::Lerp(CameraSnapshot.LensSettings.MinFocalLength, TargetLens.MinFocalLength, Alpha);
+	TargetLens.MaxFocalLength = FMath::Lerp(CameraSnapshot.LensSettings.MaxFocalLength, TargetLens.MaxFocalLength, Alpha);
+	TargetLens.MinFStop = FMath::Lerp(CameraSnapshot.LensSettings.MinFStop, TargetLens.MinFStop, Alpha);
+	TargetLens.MaxFStop = FMath::Lerp(CameraSnapshot.LensSettings.MaxFStop, TargetLens.MaxFStop, Alpha);
+	TargetLens.MinimumFocusDistance = FMath::Lerp(CameraSnapshot.LensSettings.MinimumFocusDistance, TargetLens.MinimumFocusDistance, Alpha);
+	TargetLens.SqueezeFactor = FMath::Lerp(CameraSnapshot.LensSettings.SqueezeFactor, TargetLens.SqueezeFactor, Alpha);
+	//DiaphragmBladeCount is left out
+	
+	GetCineCameraComponent()->SetLensSettings(TargetLens);
+	// LensSettings end
+	
+	// FocusSettings start
+	FCameraFocusSettings TargetFocus = TargetCamera->GetCineCameraComponent()->FocusSettings;
+
+	TargetFocus.ManualFocusDistance = FMath::Lerp(CameraSnapshot.FocusSettings.ManualFocusDistance, TargetFocus.ManualFocusDistance, Alpha);
+	TargetFocus.FocusSmoothingInterpSpeed = FMath::Lerp(CameraSnapshot.FocusSettings.FocusSmoothingInterpSpeed, TargetFocus.FocusSmoothingInterpSpeed, Alpha);
+	TargetFocus.FocusOffset = FMath::Lerp(CameraSnapshot.FocusSettings.FocusOffset, TargetFocus.FocusOffset, Alpha);
+
+	GetCineCameraComponent()->SetFocusSettings(TargetFocus);
+	// FocusSettings end
+	
+	//  CropSettings start
+	FPlateCropSettings TargetCrop = TargetCamera->GetCineCameraComponent()->CropSettings;
+
+	TargetCrop.AspectRatio = FMath::Lerp(CameraSnapshot.CropSettings.AspectRatio, TargetCrop.AspectRatio, Alpha);
+
+	//todo disabled GetCineCameraComponent()->SetCropSettings(TargetCrop);
+	// CropSettings end
+
+	// Other params start
+	GetCineCameraComponent()->bOverride_CustomNearClippingPlane = TargetCamera->GetCineCameraComponent()->bOverride_CustomNearClippingPlane;
+	GetCineCameraComponent()->SetCurrentFocalLength(FMath::Lerp(CameraSnapshot.CurrentFocalLength, TargetCamera->GetCineCameraComponent()->CurrentFocalLength, Alpha));
+	GetCineCameraComponent()->SetCurrentAperture(FMath::Lerp(CameraSnapshot.CurrentAperture, TargetCamera->GetCineCameraComponent()->CurrentAperture, Alpha));
+	GetCineCameraComponent()->SetCustomNearClippingPlane(FMath::Lerp(CameraSnapshot.CustomNearClippingPlane, TargetCamera->GetCineCameraComponent()->CustomNearClippingPlane, Alpha));
+	// Other params end
+
+	GetCineCameraComponent()->MarkRenderStateDirty();
+}
+
+void ASmoothChaserCamera::OnTargetCameraChanged_Implementation(bool bSnapImmediately, float InterpolationTime)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnTargetCameraChanged::Called native implementation that is supposed to be handled in blueprints for ASmoothChaserCamera"));
 }
--- Plugins/SequencePlayTools/Source/SequencePlayTools/Public/SmoothChaserCamera.h
+++ Plugins/SequencePlayTools/Source/SequencePlayTools/Public/SmoothChaserCamera.h
@@ -3,7 +3,37 @@
 
 #include "CoreMinimal.h"
 #include "CineCameraActor.h"
+#include "CineCameraComponent.h"
+#include "CineCameraSettings.h"
 #include "SmoothChaserCamera.generated.h"
+
+struct FCameraSettingsSnapshot
+{
+	FCameraFilmbackSettings FilmbackSettings;
+	FCameraLensSettings LensSettings;
+	FCameraFocusSettings FocusSettings;
+	FPlateCropSettings CropSettings;
+	float CurrentFocalLength;
+	float CurrentAperture;
+	bool bOverride_CustomNearClippingPlane;
+	float CustomNearClippingPlane;
+
+	FCameraSettingsSnapshot() : CurrentFocalLength(0), CurrentAperture(0), bOverride_CustomNearClippingPlane(false), CustomNearClippingPlane(0) {}
+
+	FCameraSettingsSnapshot(const UCineCameraComponent* Camera)
+	{
+		if (IsValid(Camera)) return;
+
+		FilmbackSettings = Camera->Filmback;
+		LensSettings = Camera->LensSettings;
+		FocusSettings = Camera->FocusSettings;
+		CropSettings = Camera->CropSettings;
+		CurrentFocalLength = Camera->CurrentFocalLength;
+		CurrentAperture = Camera->CurrentAperture;
+		bOverride_CustomNearClippingPlane = Camera->bOverride_CustomNearClippingPlane;
+		CustomNearClippingPlane = Camera->CustomNearClippingPlane;
+	}
+};
 
 UCLASS()
 class SEQUENCEPLAYTOOLS_API ASmoothChaserCamera : public ACineCameraActor
@@ -15,11 +45,18 @@ public:
 	//todo linetrace to see if there's stuff in the middle
 	//todo timeline length (bp) should be based on distance with min and max and a multiplier
 	//todo camera snap conditions (e.g. too far, linetrace hit, etc)
-	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Smooth Chaser Camera")
+	UFUNCTION(BlueprintCallable, Category = "Smooth Chaser Camera")
 	void SetTargetCamera(ACineCameraActor* NewTargetCamera, bool bSnapImmediately = false, float InterpolationTime = 2.f);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Smooth Chaser Camera")
+	void OnTargetCameraChanged(bool bSnapImmediately = false, float InterpolationTime = 2.f);
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Smooth Chaser Camera")
 	ACineCameraActor* TargetCamera = nullptr;
-	
+
+	FCameraSettingsSnapshot CameraSnapshot;
+
+	UFUNCTION(BlueprintCallable, Category = "Smooth Chaser Camera")
+   	void LerpCameraParams(float Alpha);
 };
